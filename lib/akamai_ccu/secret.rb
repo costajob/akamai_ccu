bae214ebf7a849ccce2821b5743d79af6ff434bf
costajob <costajob@gmail.com>
2017-06-15 13:39:58 +0200

    Mike - added secret factory by files

--- lib/akamai_ccu/secret.rb
+++ lib/akamai_ccu/secret.rb
@@ -4,6 +4,29 @@ require "securerandom"
 module AkamaiCCU
   class Secret
     DIGEST = "EG1-HMAC-SHA256"
+    EQUALITY_REGEX = /\s?+=\s?/
+
+    class << self
+      private def factory(opts)
+        new(client_secret: opts.fetch("client_secret"), host: opts.fetch("host"), access_token: opts.fetch("access_token"), client_token: opts.fetch("client_token"), max_body: opts.fetch("max-body", 2048))
+      end
+
+      def by_file(name)
+        return unless File.exist?(name)
+        data = File.readlines(name).map(&:strip).reject(&:empty?).map do |entry| 
+          entry.split(EQUALITY_REGEX)
+        end
+        factory(Hash[data])
+      end
+
+      def by_edgerc(name = File.join($HOME, ".edgerc"))
+        return unless File.exist?(name)
+        data = File.readlines(name).map(&:strip)
+        data.shift
+        data.map! { |entry| entry.split(EQUALITY_REGEX) }
+        factory(Hash[data])
+      end
+    end
 
     attr_reader :host, :max_body
 
@@ -12,7 +35,7 @@ module AkamaiCCU
       @host = URI(host)
       @access_token = access_token
       @client_token = client_token
-      @max_body = max_body
+      @max_body = max_body.to_i
       @nonce = nonce
       @timestamp = AkamaiCCU.format_utc(time) 
     end
