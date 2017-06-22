aa1f2111b61f9e047fb5f91754f4128db7a0252e
costajob <costajob@gmail.com>
2017-06-22 11:20:47 +0200

    Mike - use stubs objects

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -10,8 +10,8 @@ describe AkamaiCCU::Client do
 
   it "must execute request method on POST" do
     res = client.call do |request|
-      request.body = { hostname: "bc.akamaiapibootcamp.com", objects: %w[/index.html /homepage.html] }.to_json
+      request.body = { hostname: "bc.akamaiapibootcamp.com", objects: Stubs.urls }.to_json
     end
-    res.body.must_equal "method=Stubs::HTTP::Post;uri=https://akaa-baseurl-xxx-xxx.luna.akamaiapis.net/;initheader={\"Content-Type\"=>\"application/json\"};body=\"{\\\"hostname\\\":\\\"bc.akamaiapibootcamp.com\\\",\\\"objects\\\":[\\\"/index.html\\\",\\\"/homepage.html\\\"]}\""
+    res.body.must_equal "method=Stubs::HTTP::Post;uri=https://akaa-baseurl-xxx-xxx.luna.akamaiapis.net/;initheader={\"Content-Type\"=>\"application/json\"};body=\"{\\\"hostname\\\":\\\"bc.akamaiapibootcamp.com\\\",\\\"objects\\\":[\\\"https://akaa-baseurl-xxx-xxx.luna.akamaiapis.net/index.html\\\",\\\"https://akaa-baseurl-xxx-xxx.luna.akamaiapis.net/*.js\\\"]}\""
   end
 end
--- spec/wrapper_spec.rb
+++ spec/wrapper_spec.rb
@@ -1,7 +1,6 @@
 require "helper"
 
 describe AkamaiCCU::Wrapper do
-  let(:objects) { %w[https://bc.akamaiapibootcamp.com/index.html https://bc.akamaiapibootcamp.com/homepage.html] }
   let(:wrapper) { AkamaiCCU::Wrapper.new(secret: Stubs.secret, endpoint: Stubs::Endpoint.new("staging", "invalidate", "url"), client_klass: Stubs::Client, signer_klass: Stubs::Signer, response_klass: Stubs::Response) }
 
   it "must provide API class methods" do
@@ -16,13 +15,13 @@ describe AkamaiCCU::Wrapper do
   end
 
   it "must accept a block to edit wrapper attributes" do
-    res = AkamaiCCU::Wrapper.invalidate_by_url!(objects, Stubs.secret) do |w|
+    res = AkamaiCCU::Wrapper.invalidate_by_url!(Stubs.urls, Stubs.secret) do |w|
       w.client_klass = Stubs::Client
       w.signer_klass = Stubs::Signer
       w.response_klass = Stubs::Response
     end
     res.must_be_instance_of Stubs::Response
-    res.to_s.must_equal "uri=https://akaa-baseurl-xxx-xxx.luna.akamaiapis.net/ccu/v3/invalidate/url/production;request=method=POST;path=https://akaa-baseurl-xxx-xxx.luna.akamaiapis.net/;headers=accept-encoding,accept,user-agent,Authorization;body={\"objects\":[\"https://bc.akamaiapibootcamp.com/index.html\",\"https://bc.akamaiapibootcamp.com/homepage.html\"]}"
+    res.to_s.must_equal "uri=https://akaa-baseurl-xxx-xxx.luna.akamaiapis.net/ccu/v3/invalidate/url/production;request=method=POST;path=https://akaa-baseurl-xxx-xxx.luna.akamaiapis.net/;headers=accept-encoding,accept,user-agent,Authorization;body={\"objects\":[\"https://akaa-baseurl-xxx-xxx.luna.akamaiapis.net/index.html\",\"https://akaa-baseurl-xxx-xxx.luna.akamaiapis.net/*.js\"]}"
   end
 
   it "must return early when no objects are specified" do
@@ -30,14 +29,14 @@ describe AkamaiCCU::Wrapper do
   end
 
   it "must call the client with the specified body and auth header" do
-    res = wrapper.call(objects)
+    res = wrapper.call(Stubs.urls)
     res.must_be_instance_of wrapper.response_klass
-    res.to_s.must_equal "uri=https://akaa-baseurl-xxx-xxx.luna.akamaiapis.net/ccu/v3/invalidate/url/staging;request=method=POST;path=https://akaa-baseurl-xxx-xxx.luna.akamaiapis.net/;headers=accept-encoding,accept,user-agent,Authorization;body={\"objects\":[\"https://bc.akamaiapibootcamp.com/index.html\",\"https://bc.akamaiapibootcamp.com/homepage.html\"]}"
+    res.to_s.must_equal "uri=https://akaa-baseurl-xxx-xxx.luna.akamaiapis.net/ccu/v3/invalidate/url/staging;request=method=POST;path=https://akaa-baseurl-xxx-xxx.luna.akamaiapis.net/;headers=accept-encoding,accept,user-agent,Authorization;body={\"objects\":[\"https://akaa-baseurl-xxx-xxx.luna.akamaiapis.net/index.html\",\"https://akaa-baseurl-xxx-xxx.luna.akamaiapis.net/*.js\"]}"
   end
 
   it "must allow changing the endpoint to swith API" do
     wrapper.endpoint = Stubs::Endpoint.new("production", "delete", "cpcode")
-    res = wrapper.call(objects)
-    res.to_s.must_equal "uri=https://akaa-baseurl-xxx-xxx.luna.akamaiapis.net/ccu/v3/delete/cpcode/production;request=method=POST;path=https://akaa-baseurl-xxx-xxx.luna.akamaiapis.net/;headers=accept-encoding,accept,user-agent,Authorization;body={\"objects\":[\"https://bc.akamaiapibootcamp.com/index.html\",\"https://bc.akamaiapibootcamp.com/homepage.html\"]}"
+    res = wrapper.call(Stubs.urls)
+    res.to_s.must_equal "uri=https://akaa-baseurl-xxx-xxx.luna.akamaiapis.net/ccu/v3/delete/cpcode/production;request=method=POST;path=https://akaa-baseurl-xxx-xxx.luna.akamaiapis.net/;headers=accept-encoding,accept,user-agent,Authorization;body={\"objects\":[\"https://akaa-baseurl-xxx-xxx.luna.akamaiapis.net/index.html\",\"https://akaa-baseurl-xxx-xxx.luna.akamaiapis.net/*.js\"]}"
   end
 end
